aa412c0d6987d61eed255c92e101f16961ffb78e
Keshavkaranth <keshavarkarantha@gmail.com>
2021-06-29 22:18:48 +0530

    first commit

---
+++ ekart/app/src/main/java/com/dhruva/shopping/Interface/ItemClickListner.java
@@ -0,0 +1,7 @@
+package com.dhruva.shopping.Interface;
+import android.view.View;
+
+public interface ItemClickListner
+{
+    void onClick(View view, int position, boolean isLongClick);
+}
---
+++ ekart/app/src/main/java/com/dhruva/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.dhruva.shopping.Prevalent;
+
+import com.dhruva.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
