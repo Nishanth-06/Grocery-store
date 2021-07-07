97c7275a29d79b1aad0aedd7ffd5804a6baec52b
Keshavkaranth <keshavarkarantha@gmail.com>
2021-07-07 21:36:26 +0530

    added firebase config

--- app/src/main/java/com/dhruva/shopping/Interface/ItemClickListner.java
+++
@@ -1,7 +0,0 @@
-package com.dhruva.shopping.Interface;
-import android.view.View;
-
-public interface ItemClickListner
-{
-    void onClick(View view, int position, boolean isLongClick);
-}
---
+++ app/src/main/java/com/groceryStore/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.groceryStore.shopping.Prevalent;
+
+import com.groceryStore.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
