a33d3c65b6de4ddcc17599205aa06d97081d0f95
Keshavkaranth <keshavarkarantha@gmail.com>
2021-07-25 22:25:58 +0530

    removed menu section

--- app/src/main/java/com/groceryStore/shopping/HomeActivity.java
+++ app/src/main/java/com/groceryStore/shopping/HomeActivity.java
@@ -132,25 +132,11 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -159,8 +145,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
+        System.out.println(id);
+        System.out.println(item);
 
         if (id == R.id.nav_cart) {
             Intent intent = new Intent(HomeActivity.this,CartActivity.class);
